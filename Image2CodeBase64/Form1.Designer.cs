6064d7c4b8d0ab3695603bd2845827dcb6200694
innotester <innotester@paran.com>
2014-06-06 21:34:52 +0900

    Add Image2CodeBase64

---
+++ Image2CodeBase64/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Image2CodeBase64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2CodeBase64 = new System.Windows.Forms.Button();
+            this.button2Image = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이미지 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PNG바이너리 CodeBase64 :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(452, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 400);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2CodeBase64
+            // 
+            this.button2CodeBase64.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2CodeBase64.Location = new System.Drawing.Point(387, 153);
+            this.button2CodeBase64.Name = "button2CodeBase64";
+            this.button2CodeBase64.Size = new System.Drawing.Size(43, 40);
+            this.button2CodeBase64.TabIndex = 3;
+            this.button2CodeBase64.Text = "▶";
+            this.button2CodeBase64.UseVisualStyleBackColor = true;
+            this.button2CodeBase64.Click += new System.EventHandler(this.button2CodeBase64_Click);
+            // 
+            // button2Image
+            // 
+            this.button2Image.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2Image.Location = new System.Drawing.Point(387, 229);
+            this.button2Image.Name = "button2Image";
+            this.button2Image.Size = new System.Drawing.Size(43, 40);
+            this.button2Image.TabIndex = 3;
+            this.button2Image.Text = "◀";
+            this.button2Image.UseVisualStyleBackColor = true;
+            this.button2Image.Click += new System.EventHandler(this.button2Image_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 447);
+            this.Controls.Add(this.button2Image);
+            this.Controls.Add(this.button2CodeBase64);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2CodeBase64;
+        private System.Windows.Forms.Button button2Image;
+    }
+}
+
---
+++ Image2CodeBase64/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Image2CodeBase64
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            image = new Bitmap(350, 400);
+            Graphics gx = Graphics.FromImage(image);
+            using (Pen pen = new Pen(Color.Aqua, 20))
+            using (Pen pen2 = new Pen(Color.OrangeRed, 10))
+            {
+                gx.DrawRectangle(pen, 100, 100, 100, 100);
+                gx.DrawEllipse(pen2, 200, 150, 100, 100);
+            }
+
+            pictureBox1.Image = image;
+
+        }
+
+        private void button2CodeBase64_Click(object sender, EventArgs e)
+        {
+            if (image != null)
+            {
+                byte[] result = null;
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    image.Save(stream, ImageFormat.Png);
+                    result = stream.ToArray();
+                    textBox1.Text = System.Convert.ToBase64String(result);
+                }
+            }
+        }
+
+        private void button2Image_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] png_bin = System.Convert.FromBase64String(textBox1.Text);
+                MemoryStream stream = new MemoryStream(png_bin);
+                Image im = Image.FromStream(stream, false, false);
+                image = (Bitmap)im;
+                pictureBox1.Image = image;
+                //pictureBox1.Invalidate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("바이너리가 적합하지 않습니다.");
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            const int WM_KEYDOWN = 0x100;
+            var keyCode = (Keys)(msg.WParam.ToInt32() &
+                                  Convert.ToInt32(Keys.KeyCode));
+            if ((msg.Msg == WM_KEYDOWN && keyCode == Keys.A)
+                && (ModifierKeys == Keys.Control)
+                && textBox1.Focused)
+            {
+                textBox1.SelectAll();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
